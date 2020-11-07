2ba625fbf03e3ef92e7d3c20fdcb95ff14d35b97
Artur Siedlecki <artur.siedlecki@allegro.pl>
2020-11-07 19:20:13 +0100

    Added method that normalizes date format to yyyy-mm-dd

--- dump.py
+++ dump.py
@@ -1,5 +1,6 @@
 import scraper as sc
 import math
+from datetime import datetime
 
 
 def batch_dump_parliament_votings(term=9, votings_threshold=math.inf):
@@ -26,8 +27,24 @@ def batch_dump_parliament_votings(term=9, votings_threshold=math.inf):
                     club_page.get_vote_per_person()
 
                     for person, person_vote in club_page.person_vote.items():
-                        records_list.append((day, day_values['session'], day_values['votings'],
-                                             voting, voting_values['subject'], voting_values['routine'],
-                                             voting_values['voting_nr'], club[0], person, person_vote))
+                        records_list.append({'date': normaliza_date(day), 'session': day_values['session'],
+                                             'votings': day_values['votings'], 'time': voting,
+                                             'subject': voting_values['subject'], 'routine': voting_values['routine'],
+                                             'voting_nr': voting_values['voting_nr'], 'club': club[0],
+                                             'person': person, 'vote': person_vote})
 
     return records_list
+
+
+def normaliza_date(date_str: str):
+    months_dict = {'stycznia': 1, 'lutego': 2, 'marca': 3,
+                   'kwietnia': 4, 'maja': 5, 'czerwca': 6,
+                   'lipca': 7, 'sierpnia': 8, 'września': 9,
+                   'października': 10, 'listopada': 11, 'grudnia': 12
+                   }
+
+    date_str_splitted = date_str.split(sep=' ', maxsplit=2)
+    _day = int(date_str_splitted[0])
+    _month = months_dict[date_str_splitted[1]]
+    _year = int(date_str_splitted[2][:4])
+    return datetime(year=_year, month=_month, day=_day).strftime('%Y-%m-%d')
