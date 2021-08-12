b30dea13b490c2f84b146bad74e68e5d663b0859
Artur Siedlecki <art.fanrock@gmail.com>
2021-08-12 23:42:57 +0200

    Added parameter for excluding dates. Added inserting documents to db day by day.

--- src/dataproc.py
+++ src/dataproc.py
@@ -9,13 +9,26 @@ from src import scraper as sc
 
 
 def batch_dump_parliament_votings(term:int=9, dates:Union[list, set]=[],
-                                  votings_threshold:Union[int, float]=math.inf) -> list:
+                                  votings_threshold:Union[int, float]=math.inf,
+                                  dates_to_ignore:Union[list, set]=None,
+                                  insert_to_db:bool=False) -> list:
+    assert isinstance(dates_to_ignore, (list, set)) or dates_to_ignore is None, \
+        f"days_to_ignore should be of type (list, set), yet is of type {type(dates_to_ignore)}"
+
     uri = 'agent.xsp?symbol=posglos&NrKadencji={t}'.format(t=term)
     records_list = []
     main_voting_page = sc.MainVotingPage(term=term, suffix_uri=uri)
     main_voting_page.get_dict_of_days()
 
-    for day, day_values in main_voting_page.days_dict.items():
+    if len(dates) > 0:
+        dates_to_scrape = {key:value for key, value in main_voting_page.days_dict.items() if key in dates}
+    if dates_to_ignore:
+        for day in dates_to_ignore:
+            dates_to_scrape.pop(day, None)
+
+    for day, day_values in dates_to_scrape.items():
+        day_records = []
+        print(datetime.now(), '---', day, '--- start')
         parse_by_date_bool = (day in dates) | (len(dates) == 0)
         parse_by_votings_threshold_bool = int(day_values['votings']) < votings_threshold
 
@@ -38,12 +51,21 @@ def batch_dump_parliament_votings(term:int=9, dates:Union[list, set]=[],
                     club_page.get_vote_per_person()
 
                     for person, person_vote in club_page.person_vote.items():
-                        records_list.append({'date': day, 'session': day_values['session'],
-                                             'votings': day_values['votings'], 'time': voting,
-                                             'subject': voting_values['subject'], 'routine': voting_values['routine'],
-                                             'voting_nr': voting_values['voting_nr'], 'club': club[0],
-                                             'person': person, 'vote': person_vote,
-                                             'inserted_dt': datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
+                        day_records.append({'date': day, 'session': day_values['session'],
+                                            'votings': day_values['votings'], 'time': voting,
+                                            'subject': voting_values['subject'], 'routine': voting_values['routine'],
+                                            'voting_nr': voting_values['voting_nr'], 'club': club[0],
+                                            'person': person, 'vote': person_vote,
+                                            'inserted_dt': datetime.now().strftime('%Y-%m-%d %H:%M:%S')})
+        if insert_to_db:
+            if 'client' not in locals():
+                client = MyMongoClient()
+                client.set_votes_collection()
+            client.db.coll.insert_many(documents=day_records, ordered=False)
+            print(datetime.now(), '---', day, '--- scraped & inserted')
+        else:
+            print(datetime.now(), '---', day, '--- scraped')
+        records_list.extend(day_records)
 
     return records_list
 
